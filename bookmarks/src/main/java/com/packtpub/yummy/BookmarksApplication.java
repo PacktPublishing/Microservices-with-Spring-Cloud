6a9d1ec3e03ecd269d8c21de64a58e83260466c3
Patrick Cornelissen <cornelis@pcornelissen.de>
2018-07-05 18:19:50 +0200

    Lesson 3 section 5.3 Fault tolerance

--- bookmarks/src/main/java/com/packtpub/yummy/BookmarksApplication.java
+++ bookmarks/src/main/java/com/packtpub/yummy/BookmarksApplication.java
@@ -2,10 +2,14 @@ package com.packtpub.yummy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.web.context.support.StandardServletEnvironment;
 
 @SpringBootApplication
+@EnableCircuitBreaker
+@EnableRetry
 public class BookmarksApplication {
 
     public static void main(String[] args) {
--- bookmarks/src/main/java/com/packtpub/yummy/service/RatingService.java
+++ bookmarks/src/main/java/com/packtpub/yummy/service/RatingService.java
@@ -1,9 +1,16 @@
 package com.packtpub.yummy.service;
 
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.HystrixCommandKey;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.hateoas.UriTemplate;
 import org.springframework.http.RequestEntity;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,6 +21,7 @@ import java.util.UUID;
 import static java.util.Arrays.stream;
 
 @Service
+@Slf4j
 public class RatingService {
     @Autowired
     @LoadBalanced
@@ -21,13 +29,37 @@ public class RatingService {
     @Autowired
     HttpServletRequest request;
 
+    @SneakyThrows
+    @Retryable(maxAttempts = 5, backoff = @Backoff(multiplier = 2, random = true))
     public void removeRatings(UUID id) {
         RequestEntity entity = RequestEntity
                 .delete(new UriTemplate("http://ratings/votes/bookmark/{id}")
                         .expand(id))
                 .header("COOKIE", "SESSION=" + getCookieValue("SESSION"))
                 .build();
-        restTemplate.exchange(entity, Void.class);
+        new HystrixCommand<Void>(
+                HystrixCommand.Setter
+                        .withGroupKey(HystrixCommandGroupKey.Factory.asKey("ratings"))
+                        .andCommandKey(HystrixCommandKey.Factory.asKey("removeRatings"))
+        ) {
+            @Override
+            protected Void run() throws Exception {
+                restTemplate.exchange(entity, Void.class);
+                return null;
+            }
+
+            @Override
+            protected Void getFallback() {
+                Exception exception = executionResult.getException();
+                if (exception == null) exception = executionResult.getExecutionException();
+                if (exception == null) {
+                    throw new RuntimeException("Oh no! result: " + executionResult);
+                } else {
+                    log.info("Oh no! {}", exception.getMessage());
+                    throw new RuntimeException(exception);
+                }
+            }
+        }.run();
     }
 
     private String getCookieValue(String name) {
