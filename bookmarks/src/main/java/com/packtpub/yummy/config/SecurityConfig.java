03c7cd96831d70deaede5fd43de5f5e9c1fe08c2
Patrick Cornelissen <cornelis@pcornelissen.de>
2018-07-14 20:50:15 +0200

    (CLEANUP) fix test by allowing access to the service document without authentication

--- bookmarks/src/main/java/com/packtpub/yummy/config/SecurityConfig.java
+++ bookmarks/src/main/java/com/packtpub/yummy/config/SecurityConfig.java
@@ -19,7 +19,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/actuator/**").hasAnyRole("ADMIN")
+                .antMatchers("/").permitAll()
+                .antMatchers("/actuator/**").hasAnyRole("ADMIN")
                 .anyRequest().authenticated()
                 .and().httpBasic()
                 .and().csrf().disable();
