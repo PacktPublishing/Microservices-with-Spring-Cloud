00aa215780c1fe51cf6075e43ffbb2263165f28f
Patrick Cornelissen <cornelis@pcornelissen.de>
2018-07-16 18:06:21 +0200

    (CLEANUP) Fix active profile detection that broke during the spring boot 2 upgrade in the bookmarks service

--- bookmarks/src/main/java/com/packtpub/yummy/BookmarksApplication.java
+++ bookmarks/src/main/java/com/packtpub/yummy/BookmarksApplication.java
@@ -1,5 +1,6 @@
 package com.packtpub.yummy;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
@@ -7,23 +8,32 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.web.context.support.StandardServletEnvironment;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 @EnableCircuitBreaker
 @EnableRetry
+@Slf4j
 public class BookmarksApplication {
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(BookmarksApplication.class);
-        if(new StandardServletEnvironment().getActiveProfiles().length==0){
+        if (noActiveProfiles(args)) {
             app.setAdditionalProfiles("dev", System.getProperty("user.name"));
         }
         ConfigurableApplicationContext context = app.run(args);
 
-        System.out.println();
-        System.out.println("#######################");
-        System.out.println("##### Initialized! ####");
-        System.out.println("#######################");
-        System.out.println(" go to: http://localhost:8080");
+        log.info("");
+        log.info("#######################");
+        log.info("##### Initialized! ####");
+        log.info("#######################");
+        log.info(" go to: http://localhost:8080");
+    }
+
+    private static boolean noActiveProfiles(String[] args) {
+        return new StandardServletEnvironment().getActiveProfiles().length == 0
+                && Arrays.stream(args)
+                .noneMatch(param -> param.startsWith("--spring.profiles.active"));
     }
 
 }
